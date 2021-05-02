7217c2a0fe6a154d9d0a7a9500f0b89dd1721425
Luka Celebic <luka.celebic12@gmail.com>
2021-05-02 18:30:33 +0200

    Logic fixing 2

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 // Load all already existing divs from localStorage on page load
-let divCollection = JSON.parse(localStorage.getItem('divCollection')) || []
+let divCollection = JSON.parse(localStorage.getItem('divCollection')) || [];
 for (i = 0; i < divCollection.length; i++) {
     kreirajDiv(divCollection[i]);
 
+}
+
+function sacuvajULocalStorage(divPodaci) {
+    let pravi = true;
+    // Check if div with inputed text already exists and don't create another one if it does
+    for (i = 0; i < divCollection.length; i++) {
+        if (divCollection[i].komentar == divPodaci.komentar) {
+            pravi = false;
+            break;
+        }
+    }
+    if (pravi) {
+        divCollection.push(divPodaci);
+        localStorage.setItem('divCollection', JSON.stringify(divCollection));
+        kreirajDiv(divPodaci);
+    }
 }
 
 function kreirajDiv(divPodaci) {
@@ -20,48 +36,6 @@ function kreirajDiv(divPodaci) {
     block.appendChild(para);
 }
 
-function sacuvajULocalStorage(divPodaci) {
-
-    //     if localstorage.divCollection exists
-    //     localstorage.get
-    //     let divCollection = JSON.parse
-    //     for loop, prolazimo kroz svaki element arraya tj objekat i pitas da li objekat.komentar = divPodaci.komentar, ako jeste onda se ne pravi ponovo, ako nije onda pravi
-
-    //     divCollection.push(divPodaci);
-    //     localstorage.set(JSON.stringify(divCollection));
-
-    // else napravi ga
-    //     let divCollection = [];
-    //     divCollection.push(divPodaci);
-    //     localstorage.set(JSON.stringify(divCollection));
-
-    if (localStorage.divCollection !== undefined) {
-        let pravi = false
-        for (i = 0; i < divCollection.length; i++) {
-            if (divCollection[i].komentar == divPodaci.komentar) {
-                pravi = false
-                break
-            }
-            else {
-                pravi = true
-            }
-        }
-        if (pravi == true) {
-            divCollection.push(divPodaci)
-            localStorage.setItem('divCollection', JSON.stringify(divCollection))
-            kreirajDiv(divPodaci)
-        }
-        else {
-        }
-    }
-    else {
-        divCollection.push(divPodaci)
-        console.log(divCollection)
-        localStorage.setItem('divCollection', JSON.stringify(divCollection))
-    }
-
-}
-
 // Potvrdi button
 document.querySelector(".potvrdi").addEventListener("click", e => {
     // Take values from from, create div on page and save it to localStorage
@@ -71,7 +45,6 @@ document.querySelector(".potvrdi").addEventListener("click", e => {
         boja: document.getElementById('boje').value,
         komentar: document.getElementById('komentar').value
     };
-    kreirajDiv(divPodaci);
     sacuvajULocalStorage(divPodaci);
 });
 
